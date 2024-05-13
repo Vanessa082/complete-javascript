af08c695b486e222b36123ca3e320301b93cace2
vanessa082 <wahvanessa22@gmail.com>
2024-05-13 20:00:38 +0100

    created varaible and tried to understanding cleeancode when naming variablles

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -2,3 +2,19 @@ let js = 'amazing'
 if(js === 'amazing') alert('Jss is fun')
 
   console.log(40+8+23-10)
+
+  // data type
+  let firstName = 'van'
+  console.log(firstName)
+
+  let PI = 3.1415
+
+  // write your code to be discriptive
+
+
+  let myFirstSchool = 'sTACO'
+  let mySecondSchool = 'Rebase'
+
+  // instead of 
+  let school1 = 'Staco'
+  let school2 = 'Rebase'
